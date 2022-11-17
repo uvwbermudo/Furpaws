c8af4e30b6d06c037bcadb449e8e126b7c1ac3d7
Bermudo, Urel <urelvanwilliam.bermudo@g.msuiit.edu.ph>
2022-11-17 21:56:52 +0800

    FUR-11 landing, sign-up,log-in page

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
 from config import DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, SECRET_KEY
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_utils import create_database, database_exists
+import pymysql
 
+
+db_url = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}" 
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
@@ -9,7 +15,6 @@ def create_app():
     from .auth import auth
 
     app.register_blueprint(auth, url_prefix='/')
-
 
 
 
